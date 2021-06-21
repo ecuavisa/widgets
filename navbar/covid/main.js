7d8970984f9b73f350cfc1286c1d7c9bc3280eaa
Derian Pinto <pintoderian@gmail.com>
2021-06-21 11:10:23 -0500

    covid en tablas

---
+++ navbar/covid/main.js
@@ -0,0 +1,12 @@
+var principalSheet = 'https://docs.google.com/spreadsheets/d/1IGjMu_9US4IsvOjBsjVLeEml1EaISvHXzev-xq50X9o/edit#gid=0';
+
+$('#principalSheet').sheetrock({
+    url: principalSheet
+});
+
+
+var generalSheet = "https://docs.google.com/spreadsheets/d/1IGjMu_9US4IsvOjBsjVLeEml1EaISvHXzev-xq50X9o/edit#gid=883324876";
+
+$('#generalSheet').sheetrock({
+    url: generalSheet
+});
