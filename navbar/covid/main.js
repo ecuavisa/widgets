5ec2f84e1dfbcd3a96556e7e3f0c9181e5452a79
Derian Pinto <pintoderian@gmail.com>
2021-06-21 14:57:14 -0500

    variables

--- navbar/covid/main.js
+++ navbar/covid/main.js
@@ -1,8 +1,18 @@
 var principalSheet = 'https://docs.google.com/spreadsheets/d/1IGjMu_9US4IsvOjBsjVLeEml1EaISvHXzev-xq50X9o/edit#gid=0';
-
+window.valObject = {};
 $('#principalSheet').sheetrock({
-    url: principalSheet
+    url: principalSheet,
+    callback: function (error, options, response) {
+        let principalData = response.rows[1];
+        for (let index = 0; index < principalData.labels.length; index++) {
+            const key = principalData.labels[index];
+            const value = principalData.cellsArray[index];
+            valObject[key] = value;
+        }
+        console.log(valObject);
+    }
 });
+
 
 /*
 var generalSheet = "https://docs.google.com/spreadsheets/d/1IGjMu_9US4IsvOjBsjVLeEml1EaISvHXzev-xq50X9o/edit#gid=883324876";
@@ -11,3 +21,4 @@ $('#generalSheet').sheetrock({
     url: generalSheet
 });
 */
+
