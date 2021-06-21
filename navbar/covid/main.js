1ecb4be40ec8bcb49087a61c3f4593688fb3712f
Derian Pinto <pintoderian@gmail.com>
2021-06-21 14:28:39 -0500

    sin la segunda tabla

--- navbar/covid/main.js
+++ navbar/covid/main.js
@@ -4,9 +4,10 @@ $('#principalSheet').sheetrock({
     url: principalSheet
 });
 
-
+/*
 var generalSheet = "https://docs.google.com/spreadsheets/d/1IGjMu_9US4IsvOjBsjVLeEml1EaISvHXzev-xq50X9o/edit#gid=883324876";
 
 $('#generalSheet').sheetrock({
     url: generalSheet
 });
+*/
