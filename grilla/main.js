dd6402c6cbe856c991128d1e48d0002c6e702ec0
Eri Vera <eriveraec@gmail.com>
2021-07-08 19:45:05 -0500

    update custom

--- grilla/main.js
+++ grilla/main.js
@@ -57,24 +57,38 @@ $('#martes-data').sheetrock({
         /*Se eliminan los primeros 2*/
         rows.splice(0, 2);
         let rowIndex = 2;
-        let horaActual = moment().format("H");
+        let horaActual = parseInt(moment().format("H"));
         let minutActual = moment().format("mm");
-        rows.forEach((row, i) => {
+        let contador = 1;
+        for (let index = 0; index < rows.length; index++) {
+            const row = rows[index];
+            if (contador < (rows.length - 1)) {
+                contador++;
+            } else {
+                contador = (rows.length - 1);
+            }
             let horaStr = row.c[0].v;
-            var hora = moment(horaStr, "H").format("H");
-            var minuto = moment(horaStr, "mm").format("mm");
+            let nombre = row.c[1].v;
+            var hora = parseInt(moment(horaStr, "H").format("H"));
+            var minuto = moment(horaStr, "HH:mm:ss").minutes();
             /**
              * Siguiente
              */
-            let horaStrSiguiente = rows[i].c[0].v;
-            var horaSiguiente = moment(horaStrSiguiente, "H").format("H");
-            var minutoSiguiente = moment(horaStrSiguiente, "mm").format("mm");
-
-            if (hora == horaActual && horaSiguiente == horaActual && minuto >= minutActual && minutoSiguiente <= minutoSiguiente) {
-                $('#martes-data tr').eq(rowIndex).addClass("bg-danger");
+            let tempRow = rows[contador];
+            let horaStrSiguiente = tempRow.c[0].v;
+            var horaSiguiente = parseInt(moment(horaStrSiguiente, "H").format("H"));
+            var minutoSiguiente = moment(horaStrSiguiente, "HH:mm:ss").minutes();
+            var nombreSiguiente = tempRow.c[1].v;
+            //console.log("Hora:" + hora);
+            console.log(hora == horaActual && horaSiguiente == horaActual);
+            //console.log("Hora actual:" + horaActual);
+            if (hora == horaActual && horaActual <= horaSiguiente && nombre == nombreSiguiente) {
+                console.log("si paso martes");
+                $('#martes-data tr').eq(rowIndex).addClass("bg-success");
             }
             rowIndex++;
-        });
+        }
+        
     }
 });
 
@@ -88,15 +102,38 @@ $('#miercoles-data').sheetrock({
         /*Se eliminan los primeros 2*/
         rows.splice(0, 2);
         let rowIndex = 2;
-        let horaActual = moment().format("H");
-        rows.forEach(row => {
+        let horaActual = parseInt(moment().format("H"));
+        let minutActual = moment().format("mm");
+        let contador = 1;
+        for (let index = 0; index < rows.length; index++) {
+            const row = rows[index];
+            if (contador < (rows.length - 1)) {
+                contador++;
+            } else {
+                contador = (rows.length - 1);
+            }
             let horaStr = row.c[0].v;
-            var hora = moment(horaStr, "H").format("H");
-            if (hora == horaActual) {
-                $('#miercoles-data tr').eq(rowIndex).addClass("bg-danger");
+            let nombre = row.c[1].v;
+            var hora = parseInt(moment(horaStr, "H").format("H"));
+            var minuto = moment(horaStr, "HH:mm:ss").minutes();
+            /**
+             * Siguiente
+             */
+            let tempRow = rows[contador];
+            let horaStrSiguiente = tempRow.c[0].v;
+            var horaSiguiente = parseInt(moment(horaStrSiguiente, "H").format("H"));
+            var minutoSiguiente = moment(horaStrSiguiente, "HH:mm:ss").minutes();
+            var nombreSiguiente = tempRow.c[1].v;
+            //console.log("Hora:" + hora);
+            console.log(hora == horaActual && horaSiguiente == horaActual);
+            //console.log("Hora actual:" + horaActual);
+            if (hora == horaActual && horaActual <= horaSiguiente && nombre == nombreSiguiente) {
+                console.log("si paso miercoles");
+                $('#miercoles-data tr').eq(rowIndex).addClass("bg-success");
             }
             rowIndex++;
-        });
+        }
+        
     }
 
 });
