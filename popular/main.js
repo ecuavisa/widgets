027bceca9e2c6ab4b3725ff5e7d666a65f723991
Derian Pinto <pintoderian@gmail.com>
2021-07-16 11:33:35 -0500

    popular

--- popular/main.js
+++ popular/main.js
@@ -3,7 +3,7 @@ function dateToYMD(date) {
     var d = date.getDate();
     var m = date.getMonth() + 1; //Month from 0 to 11
     var y = date.getFullYear();
-    return '' + y + '-' + (m <= 9 ? '0' + m : m) + '-' + (d <= 9 ? '0' + d : d);
+    return '' + (d <= 9 ? '0' + d : d) + '/' + (m <= 9 ? '0' + m : m) + '/' + y;
 }
 
 function reproductor(id, serieid = "") {
