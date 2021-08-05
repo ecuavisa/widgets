41294df89ac711d22932895867be422aca6cf090
Eri Vera <eriveraec@gmail.com>
2021-08-05 12:33:11 -0500

    main

--- popular/main.js
+++ popular/main.js
@@ -217,11 +217,13 @@ function getShows() {
                 infinite: true,
                 slidesToShow: 1,
                 slidesToScroll: 1,
+                prevArrow: false,
+                nextArrow: false
                 
             });
 
             $("#showsCovers").slick({
-                infinite: true,
+                infinite: false,
                 slidesToShow: 6,
                 slidesToScroll: 3,
                 responsive: [
@@ -229,9 +231,7 @@ function getShows() {
                       breakpoint: 1024,
                       settings: {
                         slidesToShow: 3,
-                        slidesToScroll: 3,
-                        infinite: true,
-                        dots: true
+                        slidesToScroll: 3,                        
                       }
                     },
                     {
