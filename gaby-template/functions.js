f2569880965fa43bcaef3b72257d2f7d4b64d6ec
Eri Vera <eriveraec@gmail.com>
2021-09-16 17:45:03 -0500

    gaby template

---
+++ gaby-template/functions.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+
+    var d = new Date();
+    var n = d.getDay();
+    var hour = d.getHours();
+    var min = d.getMinutes();
+
+    console.log(hour,min);
+    
+    if ((hour >= 17 && min <= 36) || (hour < 18) ) 
+     { 
+        $('#envivo').addClass('parpadea');
+        $(".inner").append(`
+        
+        <img src="https://i.imgur.com/RcqFJuy.png" alt="imagen">
+        <div> estamos presentado</div>
+        ` );
+
+      }
+
+    else {
+       $('#envivo').remove('parpadea');
+       }
+
+});
