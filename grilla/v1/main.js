1b356b4eee9566ac863a83f8bb8df1fd0ffec5f1
eriveraec <eriveraec@gmail.com>
2022-02-14 23:37:08 -0500

    cuek

--- grilla/v1/main.js
+++ grilla/v1/main.js
@@ -1,14 +1,19 @@
 var principalSheet = 'https://docs.google.com/spreadsheets/d/1Fw7a5tjulR1_wtB7nlTaI3I4kbb1y5iIjnwyhGZZnmo/edit#gid=0';
 //window.valObject = {};
+
+var f = new Date();
+
+let feAc = (f.getDate() + "/" + (f.getMonth() +1) + "/" + f.getFullYear());
+console.log(feAc);
 $('#principalSheet').sheetrock({
     url: principalSheet,
 
     query: "select A,B,C",
-    fetchSize: 50,
+    fetchSize: 100,
     callback: function (data, sheetrock, response) {
         let rows = response.raw.table.rows;
         /*Se eliminan los primeros 2*/
-        rows.splice(0, 2);
+        rows.splice(0, 1);
         let rowIndex = 2;
         let horaActual = parseInt(moment().format("H"));
         let minutActual = moment().format("mm");
@@ -21,24 +26,25 @@ $('#principalSheet').sheetrock({
             } else {
                 contador = (rows.length - 1);
             }
-            let horaStr = row.c[0].v;
+            let horaStr = row.c[0].f;
             let nombre = row.c[1].v;
-            var hora = parseInt(moment(horaStr, "H").format("H"));
-            var minuto = moment(horaStr, "HH:mm:ss").minutes();
+            var hora = parseInt(moment(horaStr, "HH:mm").format("H"));
+            var minuto = moment(horaStr, "HH:mm").minutes();
+            console.log(`la hora---->:${hora}`);
             /**
              * Siguiente
              */
             let tempRow = rows[contador];
-            let horaStrSiguiente = tempRow.c[0].v;
+            let horaStrSiguiente = tempRow.c[0].f;
             var horaSiguiente = parseInt(moment(horaStrSiguiente, "H").format("H"));
             var minutoSiguiente = moment(horaStrSiguiente, "HH:mm:ss").minutes();
             var nombreSiguiente = tempRow.c[1].v;
-            //console.log("Hora:" + hora);
-            console.log(hora == horaActual && horaSiguiente == horaActual);
+            // console.log("Hora:" + hora);
+            // console.log('hora inicio - lunes',hora == horaActual && horaSiguiente == horaActual);
             //console.log("Hora actual:" + horaActual);
             if (hora == horaActual && horaActual <= horaSiguiente && nombre == nombreSiguiente) {
-                console.log("si paso");
-                $('#principalSheet tr').eq(rowIndex).addClass("bg-success");
+              
+                $('#principalSheet tr').eq(rowIndex).addClass("bg-warning");
             }
             rowIndex++;
         }
@@ -55,7 +61,7 @@ $('#martes-data').sheetrock({
     callback: function (data, sheetrock, response) {
         let rows = response.raw.table.rows;
         /*Se eliminan los primeros 2*/
-        rows.splice(0, 2);
+        rows.splice(0, 1);
         let rowIndex = 2;
         let horaActual = parseInt(moment().format("H"));
         let minutActual = moment().format("mm");
@@ -67,7 +73,7 @@ $('#martes-data').sheetrock({
             } else {
                 contador = (rows.length - 1);
             }
-            let horaStr = row.c[0].v;
+            let horaStr = row.c[0].f;
             let nombre = row.c[1].v;
             var hora = parseInt(moment(horaStr, "H").format("H"));
             var minuto = moment(horaStr, "HH:mm:ss").minutes();
@@ -75,7 +81,7 @@ $('#martes-data').sheetrock({
              * Siguiente
              */
             let tempRow = rows[contador];
-            let horaStrSiguiente = tempRow.c[0].v;
+            let horaStrSiguiente = tempRow.c[0].f;
             var horaSiguiente = parseInt(moment(horaStrSiguiente, "H").format("H"));
             var minutoSiguiente = moment(horaStrSiguiente, "HH:mm:ss").minutes();
             var nombreSiguiente = tempRow.c[1].v;
@@ -83,8 +89,8 @@ $('#martes-data').sheetrock({
             console.log(hora == horaActual && horaSiguiente == horaActual);
             //console.log("Hora actual:" + horaActual);
             if (hora == horaActual && horaActual <= horaSiguiente && nombre == nombreSiguiente) {
-                console.log("si paso martes");
-                $('#martes-data tr').eq(rowIndex).addClass("bg-success");
+                
+                $('#martes-data tr').eq(rowIndex).addClass("bg-warning");
             }
             rowIndex++;
         }
@@ -128,8 +134,54 @@ $('#miercoles-data').sheetrock({
             console.log(hora == horaActual && horaSiguiente == horaActual);
             //console.log("Hora actual:" + horaActual);
             if (hora == horaActual && horaActual <= horaSiguiente && nombre == nombreSiguiente) {
-                console.log("si paso miercoles");
-                $('#miercoles-data tr').eq(rowIndex).addClass("bg-success");
+                
+                $('#miercoles-data tr').eq(rowIndex).addClass("bg-warning");
+            }
+            rowIndex++;
+        }
+        
+    }
+
+});
+
+
+$('#jueves-data').sheetrock({
+    url: principalSheet,
+
+    query: "select J,K,L",
+    fetchSize: 50,
+    callback: function (data, sheetrock, response) {
+        let rows = response.raw.table.rows;
+        /*Se eliminan los primeros 2*/
+        rows.splice(0, 2);
+        let rowIndex = 2;
+        let horaActual = parseInt(moment().format("H"));
+        let minutActual = moment().format("mm");
+        let contador = 1;
+        for (let index = 0; index < rows.length; index++) {
+            const row = rows[index];
+            if (contador < (rows.length - 1)) {
+                contador++;
+            } else {
+                contador = (rows.length - 1);
+            }
+            let horaStr = row.c[0].v;
+            let nombre = row.c[1].v;
+            var hora = parseInt(moment(horaStr, "H").format("H"));
+            var minuto = moment(horaStr, "HH:mm:ss").minutes();
+            /**
+             * Siguiente
+             */
+            let tempRow = rows[contador];
+            let horaStrSiguiente = tempRow.c[0].v;
+            var horaSiguiente = parseInt(moment(horaStrSiguiente, "H").format("H"));
+            var minutoSiguiente = moment(horaStrSiguiente, "HH:mm:ss").minutes();
+            var nombreSiguiente = tempRow.c[1].v;
+            //console.log("Hora:" + hora);
+            console.log(hora == horaActual && horaSiguiente == horaActual);
+            //console.log("Hora actual:" + horaActual);
+            if (hora == horaActual && horaActual <= horaSiguiente && nombre == nombreSiguiente) {
+                $('#miercoles-data tr').eq(rowIndex).addClass("bg-warning");
             }
             rowIndex++;
         }
